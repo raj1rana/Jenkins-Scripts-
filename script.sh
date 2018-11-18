37fdc30f9ff31d3cd9ee1ef3ba99f49b984eb310
Rajendra Singh Rana <40059374+raj1rana@users.noreply.github.com>
2018-11-18 09:49:59 +0530

    deleting old files

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-branch="dev"
-user=uvdexdev
-ip=67.205.175.109
-if [ "$branch" = "origin/dev" ]; then
-ssh uvdexdev@25.154.252.225 "cd uvdexdev.ithands.net && git status && git pull ssh_origin staging && php artisan migrate"
-fi
