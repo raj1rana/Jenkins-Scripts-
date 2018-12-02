71b3bcc719b66a22dfb0fecfb2ad165f5985cd9f
Rajendra Singh Rana <40059374+raj1rana@users.noreply.github.com>
2018-12-02 12:51:56 +0530

    adding new methords to the build

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,61 @@
-node {
+node{
 
-  stage('Pull-msater') {
-     // pulling master from the repo
-     git 'https://github.com/raj1rana/mongoDB-docker.git'
 
-   }
-  stage('pull-staging'){
-     //pulling staging from the repo
-       git branch: 'staging', url: 'https://github.com/raj1rana/mongoDB-docker.git'
-   }
-  stage('deploy-staging') {
-     //deploy to staging server
-      sh 'rsync -avz  -e ssh --ecclude '.git/' /var/lib/jenkins/workspace/pipeline-test/.* ubuntu@13.232.107.33:/home/ubuntu/Docker-files/'
-  }
-  stage('deploy-production'){
-     //deploy to production server
-      sh 'rsync -avz -e ssh --exclude '.git'/var/lib/jenkins/workspace/pipeline-test/.* ubuntu@13.232.107.33:/home/ubuntu/master'
-  }
+    currentBuild.result = "SUCCESS"
+
+    try {
+
+
+
+        if (!env.CHNAGE_ID && env.BRANCH_NAME == 'master')
+
+           { stage('Pull-msater') {
+                 // pulling master from the repo
+                   git 'https://gitURL'
+
+                   }
+
+             stage('deploy-production'){
+                    //deploy to production server
+                   sh 'rsync -avz -e ssh --exclude /var/lib/jenkins/workspace/pipeline-test/  user@xx.xx.xx.xx.:/home/ubuntu/master'
+
+                 }
+
+            }
+
+        else {
+             stage('pull-staging'){
+                       //pulling staging from the repo
+                    git branch: 'staging', url: 'https://gitURL'
+                   }
+             stage('deploy-staging') {
+                     //deploy to staging server
+                   sh 'rsync -avz  -e ssh --exclude .git  /var/lib/jenkins/workspace/pipeline-test/  user@xx.xx.xx.xx:/home/ubuntu/Docker-files/'
+              }
+            }
+
+
+             stage('mail fail/sucess'){
+                    mail body: 'project build successful',
+                     from: 'xxxx@yyyyy.com',
+                     replyTo: 'xxxx@yyyy.com',
+                     subject: 'project build successful',
+                     to: 'yyyyy@yyyy.com'
+                 }
+  
+       }
+       catch (err) {
+
+        currentBuild.result = "FAILURE"
+
+            mail body: "project build error is here: ${env.BUILD_URL}" ,
+            from: 'xxxx@yyyy.com',
+            replyTo: 'yyyy@yyyy.com',
+            subject: 'project build failed',
+            to: 'zzzz@yyyyy.com'
+
+        throw err
+    }
 
 }
+    
